51ee886ad8148cec616eeed1676b153c059b83c9
artorias305 <105161738+artorias305@users.noreply.github.com>
2025-07-13 18:57:24 -0300

    Cat working, -b and -E flags working

---
+++ cat.c
@@ -0,0 +1,72 @@
+#include "cat.h"
+
+bool show_ends = false;
+bool number_nonblank = false;
+
+void cat_file(FILE *file, const char *filename) {
+	if (file == NULL) {
+		fprintf(stderr, "Error opening file '%s'.\n", filename);
+		return;
+	}
+	int column = 1;
+	int line = 1;
+	bool is_nonblank = false;
+	char ch;
+	while ((ch = fgetc(file)) != EOF) {
+		if (ch != '\n') {
+			is_nonblank = true;
+		}
+		if (column == 1 && number_nonblank && is_nonblank) {
+			printf("%6d\t", line);
+		}
+		if (ch == '\n' && show_ends) {
+			printf("$");
+		}
+		printf("%c", ch);
+		if (ch == '\n') {
+			column = 1;
+			line++;
+			is_nonblank = false;
+		} else {
+			column++;
+		}
+	}
+	fclose(file);
+}
+
+bool is_flag(const char *argv) {
+	if (argv[0] == '-' && argv[1] != '\0') {
+		return true;
+	}
+	return false;
+}
+
+void parse_flag(const char *arg) {
+	for (size_t i = 1; i < strlen(arg); i++) {
+		if (arg[i] == 'E') {
+			show_ends = true;
+		}
+		if (arg[i] == 'b') {
+			number_nonblank = true;
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		printf("Usage: cat [-E] [FILE]...\n");
+		exit(EXIT_SUCCESS);
+	}
+	for (int i = 0; i < argc; i++) {
+		if (is_flag(argv[i])) {
+			parse_flag(argv[i]);
+		}
+	}
+	for (int i = 1; i < argc; i++) {
+		if (!is_flag(argv[i])) {
+			FILE *file = fopen(argv[i], "r");
+			cat_file(file, argv[i]);
+		}
+	}
+	return 0;
+}
---
+++ cat.h
@@ -0,0 +1,9 @@
+#ifndef CAT_H_
+#define CAT_H_
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif // CAT_H_
